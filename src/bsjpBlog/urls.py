51c939539529032e5e3c76206458372243717983
Kamil Kindziuk <kamil.kindziuk@gmail.com>
2018-08-12 22:15:48 +0200

    add post basic view

--- src/bsjpBlog/urls.py
+++ src/bsjpBlog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts.views import PostView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('post/', PostView.as_view(), name='post')
 ]
