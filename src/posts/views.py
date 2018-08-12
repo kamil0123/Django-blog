edfc11fcc119e4815a34af48513af2577f1d2173
Kamil Kindziuk <kamil.kindziuk@gmail.com>
2018-08-13 00:06:19 +0200

    add post basic template

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+
+from .models import Post
 
 # Create your views here.
 class PostView(View):
   def get(self, request, *args, **kwargs):
-    return HttpResponse("<h1>Post</h1>")
+    queryset = Post.objects.all()
+    context = {
+      "object_list" : queryset
+    }
+    return render(request, "index.html", context)
 
