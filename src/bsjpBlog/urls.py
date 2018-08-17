b763467cd8d115cc9966a6371e38e9f2c75b6d1b
Kamil Kindziuk <kamil.kindziuk@gmail.com>
2018-08-17 20:22:22 +0200

    setup static files and css

--- src/bsjpBlog/urls.py
+++ src/bsjpBlog/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from posts.views import PostView, PostsView
@@ -22,3 +24,6 @@ urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
 
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
