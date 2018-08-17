d3505bb13e4d613914286c9fd8d18c001a1f293f
Kamil Kindziuk <kamil.kindziuk@gmail.com>
2018-08-18 00:15:59 +0200

    add imageField to post

--- src/bsjpBlog/settings.py
+++ src/bsjpBlog/settings.py
@@ -125,4 +125,6 @@ STATICFILES_DIRS = [
     #'/var/www/static/',
 ]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
--- src/posts/models.py
+++ src/posts/models.py
@@ -3,9 +3,21 @@ from django.urls import reverse
 
 # Create your models here.
 
+def upload_location(instance, filename):
+    #filebase, extension = filename.split(".")
+    #return "%s/%s.%s" %(instance.id, instance.id, extension)
+    return "%s/%s" %(instance.id, filename)
+
 class Post(models.Model):
   title = models.CharField(max_length=160)
   content = models.TextField()
+  image = models.ImageField(upload_to=upload_location, 
+            null=True, 
+            blank=True, 
+            width_field="width_field", 
+            height_field="height_field")
+  height_field = models.IntegerField(default=0)
+  width_field = models.IntegerField(default=0)
   updated = models.DateTimeField(auto_now=True, auto_now_add=False)
   created = models.DateTimeField(auto_now=False, auto_now_add=True)
 
