b6ccc3a233ec50488df17fd1fff3f3b3fdf32db4
Kamil Kindziuk <kamil.kindziuk@gmail.com>
2018-09-10 21:50:41 +0200

    few front end small changes

--- src/bsjpBlog/settings.py
+++ src/bsjpBlog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a&%xs69ai9ec%d58!kjcicko8k-e-kn_le)yrqse#*$qc%z$13'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
